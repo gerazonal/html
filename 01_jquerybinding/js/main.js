5a424f22d76c81a7a5c798283786296f0eb9b2b4
y00j1n <gerazonal@gmail.com>
2025-12-24 15:34:30 +0900

    Add files via upload

---
+++ 01_jquerybinding/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    // 코드 작성
+    // 버튼 click을 클릭하면 div박스의 내부색깔을 빨간색으로 변경
+
+    // 버튼을 클릭하면
+    $('button').click(function(){
+        // 버튼을 클릭하면 div박스의 내부색을 빨강으로 변경
+        $('div').css({"background":"red"})
+    })
+})
