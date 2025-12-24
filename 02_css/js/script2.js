da864fda02797bd46b6e5ae632141caeddd3e376
y00j1n <gerazonal@gmail.com>
2025-12-24 16:50:15 +0900

    Add files via upload

---
+++ 02_css/js/script2.js
@@ -0,0 +1,10 @@
+$(function(){
+    // $("선택자").css({"속성":"값"})
+    $("ul").css({"display":"flex", "gap":20, "margin":0, "padding":0})
+    $("li").css({"list-style":"none"})
+    $("ul .red").css({"background":"red", "width":200, "height":200, "border":"1px #000 solid"})
+    $("ul .orange").css({"background":"orange", "width":200, "height":200, "border":"1px #000 solid"})
+    $("ul .yellow").css({"background":"yellow", "width":200, "height":200, "border":"1px #000 solid"})
+    $("ul .green").css({"background":"green", "width":200, "height":200, "border":"1px #000 solid"})
+
+})    
