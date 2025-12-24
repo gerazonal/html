0f21ba0ed1c950c8afcd076fd4f192998b61952b
y00j1n <gerazonal@gmail.com>
2025-12-24 16:20:47 +0900

    Add files via upload

---
+++ 02_css/js/script.js
@@ -0,0 +1,11 @@
+// jquery 준비작업
+$(function(){
+    // $("선택자").css({"속성":"값"})
+
+    $("h1").css({"color":"red"})
+    $(".date").css({"color":"#fff","background":"purple", "font-size":80})
+    $(".para").css({"border":"10px solid blue"})
+    $("ul li").css({"border":"1px red solid"})
+    $("ul li:last-child").css({"font-size":50})
+    $("a").css({"border":"5px blue solid", "padding":"15px 10px"})
+})
